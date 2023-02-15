e6c45c37b01a33acf2ddebbc6c78b2ded32cdb9b
Luciano Marcondes <lucianobm@gmail.com>
2023-02-15 23:13:45 +0000

    aws-workshop-functions-lab1

---
+++ aws-workshop-functions-lab1.py
@@ -0,0 +1,16 @@
+# A function that prints hello world
+def hello_world():
+    print('hello world')
+
+# This line calls (runs) the function
+hello_world()
+
+#%%
+
+# A function that returns hello world
+def hello_world2():
+    return 'hello world'
+
+# Assign the hello_world2() function to a variable.
+greeting = hello_world2()
+print(greeting)
